4491248a61dc7bc4e630aa98062eb691a310f683
TuanThichCodee <lyquoctuan80@gmail.com>
2025-02-07 21:51:45 +0700

    Update views.py

--- NhaKhoa/Web/views.py
+++ NhaKhoa/Web/views.py
@@ -20,7 +20,6 @@ def book_appointment(request, clinic_id):
         return redirect('home')
     return render(request, 'book_appointment.html', {'clinic': clinic})
 
-
 def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -39,14 +38,23 @@ def register_view(request):
         if form.is_valid():
             form.save()
             messages.success(request, "Tài khoản đã được tạo thành công!")
-            return redirect('login')
+            return redirect('login')  # Chuyển hướng đến trang đăng nhập
         else:
             messages.error(request, "Đăng ký thất bại. Vui lòng thử lại.")
     else:
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
 
-
 def logout_view(request):
     logout(request)
-    return render(request, 'logout.html')
+    messages.success(request, "Bạn đã đăng xuất thành công.")
+    return redirect('login')  # Chuyển hướng đến trang đăng nhập sau khi đăng xuất
+
+def datbacsi(request):
+    return render(request, 'datbacsi.html') 
+
+def datxetnghiem(request):
+    return render(request, 'datxetnghiem.html')
+
+def dattiemchung(request):
+    return render(request, 'dattiemchung.html')
