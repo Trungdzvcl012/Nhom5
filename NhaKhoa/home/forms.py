29f814ee64b8c36770ee3e77eb376ed295d8d26b
TuanThichCodee <lyquoctuan80@gmail.com>
2025-02-13 15:02:03 +0700

    Delete NhaKhoa/home directory

--- NhaKhoa/home/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Appointment
-
-class AppointmentForm(forms.ModelForm):
-    class Meta:
-        model = Appointment
-        fields = ['name', 'email', 'phone', 'date', 'time', 'notes']
--- NhaKhoa/home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-from . import views
-urlpatterns = [
-    path('book/', views.book_appointment, name='book_appointment'),
-    path('success/', views.success, name='success'),
-    path('', views.home, name='home'),
-]
