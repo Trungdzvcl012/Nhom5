806237776f2d4f28d38c73964c3233a5cc236d94
Trần Gia Huy <huytran36629@gmail.com>
2025-02-20 00:11:53 +0700

    Update urls.py

--- NhaKhoa/Web/urls.py
+++ NhaKhoa/Web/urls.py
@@ -18,4 +18,10 @@ urlpatterns = [
     path('dat-lich-kham/', views.dat_lich_kham, name='datlichkham'),
     path('tham-gia-he-thong-nature/', views.tham_gia_he_thong_nature, name='tham_gia_he_thong_nature'),
     path('ve-he-thong-y-khoa-nature/', views.ve_he_thong_y_khoa_nature, name='ve_he_thong_y_khoa_nature'),
+    path('huong_dan_dat-lich-kham/', views.huong_dan_dat_lich_kham, name='huong_dan_dat_lich_kham'),
+    path('tu-van-video/', views.tu_van_video, name='tu_van_video'),
+    path('quy-trinh-hoan-phi/', views.quy_trinh_hoan_phi, name='quy_trinh_hoan_phi'),
+    path('quy-trinh-di-kham/', views.quy_trinh_di_kham, name='quy_trinh_di_kham'),
+    path('cau-hoi-thuong-gap/', views.cau_hoi_thuong_gap, name='cau_hoi_thuong_gap'),
+
 ]
