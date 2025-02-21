342987dc89e235764085e469d1c849f784645f85
Đào Duy Cường <daoduycuong080605@gmail.com>
2025-02-22 02:10:22 +0700

    Update urls.py

--- NhaKhoa/Web/urls.py
+++ NhaKhoa/Web/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('quangcao/', views.quangcao, name='quangcao'),
     path('thanhtoan/', views.thanhtoanvienphi, name='thanhtoanvienphi'),
     path('xemchitiet/', views.xem_chi_tiet, name='xemchitiet'),
-    path('dat-lich-kham/', views.dat_lich_kham, name='datlichkham'),
+    path('dat-lich-kham/', views.dat_lich_kham, name='DatLichKham'),
     path('tham-gia-he-thong-nature/', views.tham_gia_he_thong_nature, name='tham_gia_he_thong_nature'),
     path('ve-he-thong-y-khoa-nature/', views.ve_he_thong_y_khoa_nature, name='ve_he_thong_y_khoa_nature'),
     path('huong_dan_dat-lich-kham/', views.huong_dan_dat_lich_kham, name='huong_dan_dat_lich_kham'),
@@ -23,5 +23,5 @@ urlpatterns = [
     path('quy-trinh-hoan-phi/', views.quy_trinh_hoan_phi, name='quy_trinh_hoan_phi'),
     path('quy-trinh-di-kham/', views.quy_trinh_di_kham, name='quy_trinh_di_kham'),
     path('cau-hoi-thuong-gap/', views.cau_hoi_thuong_gap, name='cau_hoi_thuong_gap'),
-
 ]
+    
