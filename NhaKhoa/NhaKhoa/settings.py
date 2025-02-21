c4ae7a7741a964b8fa3d827b90ff96ee93e147d7
Đào Duy Cường <daoduycuong080605@gmail.com>
2025-02-22 02:16:47 +0700

    Update settings.py

--- NhaKhoa/NhaKhoa/settings.py
+++ NhaKhoa/NhaKhoa/settings.py
@@ -131,3 +131,15 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = '/'
+
+AUTHENTICATION_BACKENDS = [
+    'Web.backends.SQLServerAuthBackend', # Your custom backend FIRST
+    'django.contrib.auth.backends.ModelBackend', # Django's default backend SECOND
+]
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  # Ví dụ với Gmail
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'nhakhoanature@gmail.com'  # Email của bạn
+EMAIL_HOST_PASSWORD = 'maiiuuth1@'  # Mật khẩu email của bạn
+    
